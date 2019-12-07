707d83b8a4305cb632fd28afc42e489ca5c671cd
adeak <adeak@users.noreply.github.com>
2019-12-07 22:04:56 +0100

    Day 7, all the OOPs

--- day07.py
+++ day07.py
@@ -1,21 +1,8 @@
 import operator
-from itertools import chain, cycle, permutations, count
+from itertools import chain, cycle, permutations
+from collections import deque
 
-def step_intcode(src, i, incode):
-    """Take a step in an intcode program
-
-    Input:
-        src: list of ints with instructions
-          i: current position of instruction pointer
-     incode: value for input instruction
-
-    Returns in a tuple:
-         op: str, function to call or None for exit
-     params: the operands for op if any, else []
-      i_out: index of the output if any, else None
-          i: the next index of the instruction pointer
-    """
-
+class Intcode:
     ops = {1: operator.add,
            2: operator.mul,
            3: 'in',
@@ -27,99 +14,116 @@ def step_intcode(src, i, incode):
            99: None,  # signal exit
            }
 
-    opval = src[i]
+    def __init__(self, src, inputs=None):
+        self.src = src.copy()
+        self.ip = 0
+        self.pipe = None  # potential instance to pipe output into
+        if not inputs:
+            inputs = deque([])
+        self.inputs = deque(inputs)
+        self.outputs = []
+        self.last_op = None
 
-    opcode = opval % 100
-    if opcode == 99:
-        n_inps = 0
-        n_outs = 0
-    elif opcode in [1, 2]:
-        n_inps = 2
-        n_outs = 1
-    elif opcode == 3:
-        n_inps = 0
-        n_outs = 1
-    elif opcode == 4:
-        n_inps = 1
-        n_outs = 0
-    elif opcode in [5, 6]:
-        n_inps = 2
-        n_outs = 0
-    elif opcode in [7, 8]:
-        n_inps = 2
-        n_outs = 1
-    else:
-        assert False, f'Invalid opcode {opcode} at position {i}!'
+    def pipe_into(self, other):
+        self.pipe = other
 
-    op = ops.get(opcode, -1)
-    assert op != -1, f'Need to implement more operations in ops dict!'
+    @staticmethod
+    def modes_from_op(opval):
+        """Decode parameter modes into binary ints"""
+        parammodes = chain(map(int, reversed(str(opval//100))), cycle([0])) # 1 for value, 0 for pointer
+        return parammodes
 
-    # decode parameter modes into binary ints
-    parammodes = chain(map(int, reversed(str(opval//100))), cycle([0])) # 1 for value, 0 for pointer
-
-    if op == "in":
-        params = [incode]
-    else:
-        params = [src[k] if mode else src[src[k]] for k,mode in zip(range(i + 1, i + 1 + n_inps), parammodes)]
-
-    i_out = src[i + 1 + n_inps] if n_outs else None
-
-    # handle jumps first, otherwise increment instruction pointer
-    if (op == 'jmpif' and params[0]) or (op == 'jmpifn' and not params[0]):
-        i = params[1]
-    else:
-        i += 1 + n_inps + n_outs
-
-    return op, params, i_out, i
-
-def simulate_sequence(src_orig, seq, part1=True):
-    last_output = 0
-    srcs = [src_orig.copy() for _ in range(5)]  # store code state
-    ips = [0] * 5  # store instruction pointer
-    for it in count():
-        for i_amp,(src,phase) in enumerate(zip(srcs, seq)):
-            # very first iteration per amplifier: phase
-            incode = last_output if it else phase
-
-            # load instruction pointer
-            i = ips[i_amp]
-            while True:
-                op, params, i_out, i = step_intcode(src, i, incode)
-        
-                if op is None:
-                    # exit this sequence
-                    return last_thrustput
-        
-                if op == 'out':
-                    # break out, pass on output
-                    # output is in params[0]
-                    last_output = params[0]
-                    break
-        
-                if op in ['jmpif', 'jmpifn']:
-                    # i already incremented
-                    continue
-        
-                if op == 'in':
-                    # input is in params[0]
-                    res = params[0]
+    def step(self):
+        """Take a step in the intcode program"""
+        src = self.src
+        ip = self.ip
     
-                    ## prepare for next input
-                    incode = last_output
-                else:
-                    # compute result; add/mul/lt/eq cases
-                    res = op(*params)
-        
-                src[i_out] = res
+        opval = src[ip]
+    
+        opcode = opval % 100
+        if opcode == 99:
+            n_inps = 0
+            n_outs = 0
+        elif opcode in [1, 2]:
+            n_inps = 2
+            n_outs = 1
+        elif opcode == 3:
+            n_inps = 0
+            n_outs = 1
+        elif opcode == 4:
+            n_inps = 1
+            n_outs = 0
+        elif opcode in [5, 6]:
+            n_inps = 2
+            n_outs = 0
+        elif opcode in [7, 8]:
+            n_inps = 2
+            n_outs = 1
+        else:
+            assert False, f'Invalid opcode {opcode} at position {i}!'
+    
+        op = self.ops.get(opcode, -1)
+        assert op != -1, f'Need to implement more operations in ops dict!'
 
-            ips[i_amp] = i
+        self.last_op = op
+    
+        # get position of output, if any
+        ip_out = src[ip + 1 + n_inps] if n_outs else None
+
+        # handle "input": no parameter modes
+        if op == "in":
+            src[ip_out] = self.inputs.popleft()
+    
+        parammodes = self.modes_from_op(opval)
+        params = [src[k] if mode else src[src[k]] for k,mode in zip(range(ip + 1, ip + 1 + n_inps), parammodes)]
+
+        # handle jumps, increment intruction pointer
+        if (op == 'jmpif' and params[0]) or (op == 'jmpifn' and not params[0]):
+            self.ip = params[1]
+        else:
+            self.ip += 1 + n_inps + n_outs
+    
+        # handle "output"
+        if op == "out":
+            # value is params[0]
+            res = params[0]
+            self.outputs.append(res)
+            if self.pipe:
+                self.pipe.inputs.append(res)
+    
+        # compute values for callables
+        if callable(op):
+            src[ip_out] = op(*params)
+
+        return
+
+def simulate_sequence(src, seq, part1=True):
+    instances = [Intcode(src) for _ in range(5)]
+
+    # initialize inputs with phase, connect instances
+    for i_amp,(instance,phase) in enumerate(zip(instances, seq)):
+        instance.inputs.append(phase)
+        instance.pipe_into(instances[(i_amp + 1) % 5])
+    instances[0].inputs.append(0)
+
+    while True:
+        # loop over feedback loops
+        for i_amp,instance in enumerate(instances):
+            # loop over amplifiers
+            while True:
+                # loop over instruction steps
+                instance.step()
         
-        last_thrustput = last_output
+                if instance.last_op is None:
+                    # exit this sequence for good, read last output (part 2)
+                    return instances[-1].outputs[-1]
+        
+                if instance.last_op == 'out':
+                    # hand execution over to the next amplifier
+                    break
 
         if part1:
-            break
-
-    return last_thrustput
+            return instances[-1].outputs[-1]
 
 def day07(inp):
     src = list(map(int, inp.strip().split(',')))
